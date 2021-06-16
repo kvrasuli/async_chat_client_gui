948c898c2f70e44e9adfb6d799e87c82c6d474e8
Kirill Rasuli <rasulikv@gmail.com>
2021-06-16 14:57:21 +0300

    Add cli args parsing & logging

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import asyncio
 import gui
 import time
+import logging
+import configargparse
+from dotenv import load_dotenv
+
+logger = logging.getLogger(__file__)
 
 
 async def generate_msgs(queue):
@@ -18,4 +23,23 @@ async def main():
         generate_msgs(messages_queue),
     )
 
-asyncio.run(main())
+def parse_args():
+    load_dotenv()
+    parser = configargparse.ArgParser()
+    parser.add('--host', help='address of host', env_var='CHAT_HOST')
+    parser.add('--rport', help='number of port', env_var='CHAT_PORT_TO_READ')
+    parser.add('--wport', help='number of port', env_var='CHAT_PORT_TO_WRITE')
+    parser.add('--token', help='chat token', env_var='CHAT_TOKEN')
+    parser.add('--nickname', help='your nickname', env_var='CHAT_NICKNAME')
+    parser.add('-m', '--message', help='message to send') # , required=True
+    parser.add('--log', help='enable logs', action='store_true')
+    return parser.parse_args()
+
+
+if __name__ == '__main__': 
+    args = parse_args()  
+    logging.basicConfig(format="%(levelname)s sender: %(message)s", level=logging.ERROR)
+    if args.log:
+        logging.basicConfig(level=logging.DEBUG)
+    
+    asyncio.run(main()) 
